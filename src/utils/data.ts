e41b7206abe8bfaf495195bb96fc82d4f4c65a4b
lauralen <laurallenkutyte@gmail.com>
2021-01-12 17:28:58 +0200

    Add mock data

---
+++ src/utils/data.ts
@@ -0,0 +1,24 @@
+import { Station } from "utils/types";
+
+export const stationsList: Station[] = [
+  {
+    title: "Putin FM",
+    frequency: "66,6"
+  },
+  {
+    title: "Dribbble FM",
+    frequency: "101,2"
+  },
+  {
+    title: "Doge FM",
+    frequency: "99,4"
+  },
+  {
+    title: "Ballads FM",
+    frequency: "87,1"
+  },
+  {
+    title: "Maximum FM",
+    frequency: "142,2"
+  }
+];
---
+++ src/utils/types.ts
@@ -0,0 +1,4 @@
+export interface Station {
+  title: string;
+  frequency: string;
+}

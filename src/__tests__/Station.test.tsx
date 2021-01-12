b59c26980de7b65ae219947639a7770992366e2d
lauralen <laurallenkutyte@gmail.com>
2021-01-12 20:07:19 +0200

    Display additional info for selected station

--- src/__tests__/Station.test.tsx
+++ src/__tests__/Station.test.tsx
@@ -2,9 +2,22 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import Station from "components/Station";
 
-test("renders station info", () => {
-  render(<Station title="Station FM" frequency="78,9" />);
+test("renders collapsed station info", () => {
+  render(<Station title="Station FM" frequency="78,9" isSelected={false} />);
 
   expect(screen.getByText(/station fm/i)).toBeInTheDocument;
   expect(screen.getByText(/78,9/i)).toBeInTheDocument;
+});
+
+test("renders expanded station info", () => {
+  render(<Station title="Station FM" frequency="78,9" isSelected={true} />);
+
+  expect(screen.getByText(/station fm/i)).toBeInTheDocument;
+  expect(screen.getByText(/78,9/i)).toBeInTheDocument;
+
+  const buttons = screen.queryAllByRole("button");
+  const image = screen.getByRole("img", { name: /station logo/i });
+
+  expect(buttons.length).toBe(2);
+  expect(image).toBeTruthy;
 });
--- src/components/Station.tsx
+++ src/components/Station.tsx
@@ -1,18 +1,43 @@
 import React from "react";
 import style from "./Station.module.scss";
 
+import Button from "components/Button";
+
+import minusIcon from "images/minus.png";
+import plusIcon from "images/plus.png";
+
 type Props = {
   title: string;
   frequency: string;
   isSelected: boolean;
 };
 
-function Station({ title, frequency }: Props): JSX.Element {
+function Station({ title, frequency, isSelected }: Props): JSX.Element {
   return (
-    <div className={style.station}>
-      <span>{title}</span>
-      <span className={style.bold}>{frequency}</span>
-    </div>
+    <>
+      {isSelected && (
+        <div className={style.expanded}>
+          <Button>
+            <img src={minusIcon} alt="Minus" />
+          </Button>
+
+          <img
+            className={style.img}
+            src="https://upload.wikimedia.org/wikipedia/en/6/67/ELO_Time_expanded_album_cover.jpg"
+            alt="Station logo"
+          />
+
+          <Button>
+            <img src={plusIcon} alt="Plus" />
+          </Button>
+        </div>
+      )}
+
+      <div className={style.station}>
+        <span>{title}</span>
+        <span className={style.bold}>{frequency}</span>
+      </div>
+    </>
   );
 }
 
